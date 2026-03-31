27f910bf4d3153be20a8a994bed552016b6e1aad
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-03-31 11:02:31 +0200

    Fixing globals

--- src/globals.ts
+++ src/globals.ts
@@ -1,2 +1,3 @@
 export const INBOX_URL = "http://localhost:3001/inbox/";
 //export const INBOX_URL = "https://patrickhochstenbach.net/test/";
+export const VALIDATOR_URL =  'http://localhost:3000/validate';
--- src/validate.ts
+++ src/validate.ts
@@ -1,22 +1,27 @@
-const VALIDATOR = 'http://localhost:3000/validate';
+import { VALIDATOR_URL } from "./globals";
 
 export interface ValidationResult {
     data: string;
 }
 
 export async function validateNotification(data: string) : Promise<ValidationResult> {
-    const response = await fetch(VALIDATOR, {
-        method: "POST",
-        body: data
-    });
+    try {
+        const response = await fetch(VALIDATOR_URL, {
+            method: "POST",
+            body: data
+        });
 
-    if (! response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
+        if (! response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        const json = await response.json();
+
+        return {
+            data: json.result
+        } as ValidationResult;
     }
-
-    const json = await response.json();
-
-    return {
-        data: json.result
-    } as ValidationResult;
+    catch (e) {
+        throw new Error(`Failed to contact ${VALIDATOR_URL}`);
+    }
 }
